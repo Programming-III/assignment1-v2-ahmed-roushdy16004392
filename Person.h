58cc695fa4154a25e58f6dc4e9ea0e59aea8c367
ahmed-roushdy16004392 <ahmedalaa.roushdy@gmail.com>
2025-11-11 10:00:12 +0200

    Update Person.h
    updated person file

--- Person.h
+++ Person.h
@@ -1,20 +1,23 @@
 #ifndef PERSON_H
 #define PERSON_H
+#include <iostream>
 
 #include <string>
 using namespace std;
 
 //#write Person class here
-
-
-
-
-
-
-
-
-
-
+class Person {
+    private: 
+        string name;
+        int id;
+    public:
+        Person();
+        Person(string name, int id);
+        ~Person();
+        void display();
+        string getName();
+        int getID();
+};
 
 
 #endif

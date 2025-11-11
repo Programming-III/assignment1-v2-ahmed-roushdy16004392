5afe625a5765076435eacfa87ed4d93de3c4a6a5
ahmed-roushdy16004392 <ahmedalaa.roushdy@gmail.com>
2025-11-11 09:58:03 +0200

    Update Course.h
    completed course class

--- Course.h
+++ Course.h
@@ -5,17 +5,19 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
-
-
-
-
-
-
-
-
-
-
-
+class Course{
+    private:
+        string courseCode;
+        string courseName;
+        int maxStudents;
+        Student* students;
+        int currentStudents;
+    public:
+        Course();
+        Course(string cCode, string cName, int mStudents, int currStudents);
+        void addStudent(const Student& s);
+        void displayCourseInfo();
+};
 
 
 
